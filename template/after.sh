0a9da8ea8c9a80f2d4f9d92a8e5ddea01930f333
mah60 <mah60@psu.edu>
2026-09-04 13:34:36 -0400

    Wait for AnythingLLM port

--- template/after.sh
+++ template/after.sh
@@ -1,10 +1,10 @@
 # Wait for the server to start
-echo "Waiting for server to open port ${port}..."
+echo "Waiting for server to open port ${ANYTHINGLLM_INTERNAL_PORT}..."
 
-if wait_until_port_used "${host}:${port}" 60; then
-  echo "Discovered server listening on port ${port}!"
+if wait_until_port_used "${host}:${ANYTHINGLLM_INTERNAL_PORT}" 60; then
+  echo "Discovered server listening on port ${ANYTHINGLLM_INTERNAL_PORT}!"
 else
-  echo "Timed out waiting for server to open port ${port}!"
+  echo "Timed out waiting for server to open port ${ANYTHINGLLM_INTERNAL_PORT}!"
   #pkill -P ${OLLAMA_PID}
   clean_up 1
 fi
